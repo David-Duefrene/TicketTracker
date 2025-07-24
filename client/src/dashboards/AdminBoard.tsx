d50dc96812f3d52971be7bb05944f6cb2503c181
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2025-07-24 09:15:06 -0600

    rearrancged Admin board to have mutiple tabs, each for user, group and queue management.

--- client/src/dashboards/AdminBoard.tsx
+++ client/src/dashboards/AdminBoard.tsx
@@ -5,14 +5,26 @@ import {  AddUserToGroup, CreateUser } from '../features/users/index';
 import { TabbedComponent } from '../components/TabbedComponent/index';
 
 const AdminBoard = () => {
-    const tabs: [string, React.ReactNode][] = [
+    const userTabs: [string, React.ReactNode][] = [
         ['Create User', <CreateUser key="create-user" />],
-        ['Create Group', <UserGroupCreate key="create-group" />],
-        ['Create Ticket Queue', <CreateTicketQueue key="create-ticket-queue" />],
-        ['Add User to Group', <AddUserToGroup key="add-user-to-group" />]
+        ['Add User to Group', <AddUserToGroup key="add-user-to-group" />],
     ];
 
-    return <TabbedComponent tabs={tabs} />;
+    const groupTabs: [string, React.ReactNode][] = [
+        ['Create Group', <UserGroupCreate key="create-group" />],
+    ];
+
+    const ticketQueueTabs: [string, React.ReactNode][] = [
+        ['Create Ticket Queue', <CreateTicketQueue key="create-ticket-queue" />],
+    ];
+
+    return <TabbedComponent tabs={
+        [
+            ['User Management', userTabs],
+            ['Group Management', groupTabs],
+            ['Ticket Queue Management', ticketQueueTabs],
+        ]
+    } />;
 };
 
 export default AdminBoard;
