74327a22b00f0b440c34fbbae192b84dbf8fef7d
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2025-07-24 20:51:58 -0600

    a

--- client/src/dashboards/AdminBoard.tsx
+++ client/src/dashboards/AdminBoard.tsx
@@ -1,6 +1,7 @@
 import { UserGroupCreate } from '../features/userGroups/index';
 import { CreateTicketQueue } from '../features/ticketQueue/index';
 import {  AddUserToGroup, CreateUser } from '../features/users/index';
+import CreateGroupCaseQueuePermissionJuntion from '../features/groupCaseQueuePermissionJuntion/components/CreateGroupCaseQueuePermissionJuntion';
 
 import { TabbedComponent } from '../components/TabbedComponent/index';
 
@@ -16,13 +17,14 @@ const AdminBoard = () => {
 
     const ticketQueueTabs: [string, React.ReactNode][] = [
         ['Create Ticket Queue', <CreateTicketQueue key="create-ticket-queue" />],
+        ['Create Group Case Queue Permission Junction', <CreateGroupCaseQueuePermissionJuntion key="create-group-case-queue-permission-junction" />],
     ];
 
     return <TabbedComponent tabs={
         [
-            ['User Management', userTabs],
-            ['Group Management', groupTabs],
-            ['Ticket Queue Management', ticketQueueTabs],
+            ['User Management', <TabbedComponent tabs={userTabs} key="user-management" />],
+            ['Group Management', <TabbedComponent tabs={groupTabs} key="group-management" />],
+            ['Ticket Queue Management', <TabbedComponent tabs={ticketQueueTabs} key="ticket-queue-management" />],
         ]
     } />;
 };
