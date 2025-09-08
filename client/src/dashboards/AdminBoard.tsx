0c2c15d9c3f929566c00410f5e4ace36e2c79a20
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2025-09-07 20:46:23 -0600

    added initial create ticket tab

--- client/src/dashboards/AdminBoard.tsx
+++ client/src/dashboards/AdminBoard.tsx
@@ -1,6 +1,8 @@
 import { UserGroupCreate } from '../features/userGroups/index';
 import { CreateTicketQueue } from '../features/ticketQueue/index';
 import {  AddUserToGroup, CreateUser } from '../features/users/index';
+import { CreateTicket } from '../features/tickets/index';
+
 import CreateGroupCaseQueuePermissionJuntion from '../features/groupCaseQueuePermissionJuntion/components/CreateGroupCaseQueuePermissionJuntion';
 
 import { TabbedComponent } from '../components/TabbedComponent/index';
@@ -22,11 +24,16 @@ const AdminBoard = () => {
         ['Create Group Case Queue Permission Junction', <CreateGroupCaseQueuePermissionJuntion key="create-group-case-queue-permission-junction" />],
     ];
 
+    const ticketTabs: [string, React.ReactNode][] = [
+        ['Create Ticket', <CreateTicket key="create-ticket" />],
+    ];
+
     return <TabbedComponent tabs={
         [
             ['User Management', <TabbedComponent tabs={userTabs} key="user-management" />],
             ['Group Management', <TabbedComponent tabs={groupTabs} key="group-management" />],
             ['Ticket Queue Management', <TabbedComponent tabs={ticketQueueTabs} key="ticket-queue-management" />],
+            ['Ticket Management', <TabbedComponent tabs={ticketTabs} key="ticket-management" />],
         ]
     } />;
 };
